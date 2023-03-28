d37b6fbed4d8d537adbc5cf1c89586d9ab55e61b
Caceresenzo <caceresenzo1502@gmail.com>
2023-03-28 20:17:12 +0200

    chore: bump version to 0.5.0

--- crunch_cli/__version__.py
+++ crunch_cli/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'crunch-cli'
 __description__ = 'crunch-cli - CLI of the CrunchDAO Platform'
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __author__ = 'Enzo CACERES'
 __author_email__ = 'enzo.caceres@crunchdao.com'
 __url__ = 'https://github.com/crunchdao/crunch-cli'
