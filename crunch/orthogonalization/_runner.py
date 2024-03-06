82c48c52985fdad8246734e036c227874c06b3bd
Caceresenzo <caceresenzo1502@gmail.com>
2024-03-06 18:36:46 +0100

    feat(orthogonalization/runner): add `set` and `restore` function

--- crunch/orthogonalization/_runner.py
+++ crunch/orthogonalization/_runner.py
@@ -1,7 +1,23 @@
+import typing
+
 import pandas
 
-def delegate(
-    dataframe: pandas.DataFrame,
-):
-    # runner's executor will replace it
-    raise NotImplementedError("stub")
+from .. import api
+
+
+def _not_available(dataframe: pandas.DataFrame) -> typing.List[api.Score]:
+    raise NotImplementedError("orthogonalization is not available")
+
+
+# runner's executor will replace it
+delegate = _not_available
+
+
+def set(f: typing.Callable[[pandas.DataFrame], typing.List[api.Score]]):
+    global delegate
+    delegate = f
+
+
+def restore():
+    global delegate
+    delegate = _not_available
