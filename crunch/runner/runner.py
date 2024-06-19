ebcea973f8fdc670da3e225054f748894a7cea01
Caceresenzo <caceresenzo1502@gmail.com>
2024-06-19 12:36:18 +0200

    feat(runner): add determinism check

--- crunch/runner/runner.py
+++ crunch/runner/runner.py
@@ -11,9 +11,13 @@ class Runner(abc.ABC):
 
     def __init__(
         self,
-        competition_format: api.CompetitionFormat
+        competition_format: api.CompetitionFormat,
+        determinism_check_enabled=False,
     ):
         self.competition_format = competition_format
+
+        self.determinism_check_enabled = determinism_check_enabled
+        self.deterministic = True if determinism_check_enabled else None
 
     def start(self):
         self.setup()
@@ -34,6 +38,12 @@ class Runner(abc.ABC):
 
         else:
             raise ValueError(f"unsupported: {self.competition_format}")
+        
+        if self.determinism_check_enabled:
+            if self.deterministic:
+                self.log(f"determinism check: passed")
+            else:
+                self.log(f"determinism check: failed", error=True)
 
         prediction = self.finalize(prediction)
         self.log("ended")
@@ -55,23 +65,38 @@ class Runner(abc.ABC):
             forced_train = " forced=True" if forced_train else ""
             self.log(f"looping moon={moon} train={train}{forced_train} ({index + 1}/{len(self.keys)})")
 
-            prediction = self.timeseries_loop(
-                moon,
-                train,
-            )
+            prediction = self.timeseries_loop(moon, train)
+
+            if self.deterministic:
+                prediction2 = self.timeseries_loop(moon, False)
+                self.deterministic = prediction.equals(prediction2)
+                self.log(f"deterministic: {str(self.deterministic).lower()}")
 
             predictions.append(prediction)
 
         return pandas.concat(predictions)
 
     @abc.abstractmethod
-    def start_dag(self):
-        ...
-
-    @abc.abstractmethod
     def timeseries_loop(
         self,
         moon: int,
+        train: bool
+    ) -> pandas.DataFrame:
+        ...
+
+    def start_dag(self):
+        prediction = self.dag_loop(True)
+
+        if self.deterministic:
+            prediction2 = self.dag_loop(False)
+            self.deterministic = prediction.equals(prediction2)
+            self.log(f"deterministic: {str(self.deterministic).lower()}")
+
+        return prediction
+
+    @abc.abstractmethod
+    def dag_loop(
+        self,
         train: bool
     ) -> pandas.DataFrame:
         ...
