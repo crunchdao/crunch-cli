1d01e63c0e3bbf2b88659b57e59ad647d3c8e502
Caceresenzo <caceresenzo1502@gmail.com>
2024-07-04 17:24:32 +0200

    chore(api/errors): update import list

--- crunch/api/__init__.py
+++ crunch/api/__init__.py
@@ -2,18 +2,29 @@ from .domain import *
 from .client import Client
 from . import auth
 
+# update errors list:
+# python -c 'import crunch.api.errors; print("\n".join(list(filter(lambda x: x.endswith("Exception"), vars(crunch.api.errors).keys()))))'
+
 from .errors import (
     ApiException,
     InternalServerException,
+    RetryableException,
+    AnnotatedConnectException,
+    NullPointerException,
     ValidationFailedException,
     CheckException,
     CompetitionNameNotFoundException,
     CrunchNotFoundException,
+    CurrentPhaseNotFoundException,
     DailySubmissionLimitExceededException,
     ForbiddenLibraryException,
     InvalidProjectTokenException,
     NeverSubmittedException,
     ProjectNotFoundException,
     RunNotFoundException,
+    RestrictedPhaseActionException,
     RoundNotFoundException,
+    CurrentRoundNotFoundException,
+    LatestRoundNotFoundException,
+    NextRoundNotFoundException
 )
