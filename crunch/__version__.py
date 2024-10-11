25a91a1f457f578ed3756b301bea8686daaa595f
Caceresenzo <caceresenzo1502@gmail.com>
2024-10-11 02:05:28 +0200

    release: bump to 4.0.0b9

--- crunch/__version__.py
+++ crunch/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'crunch-cli'
 __description__ = 'crunch-cli - CLI of the CrunchDAO Platform'
-__version__ = '4.0.0b8'
+__version__ = '4.0.0b9'
 __author__ = 'Enzo CACERES'
 __author_email__ = 'enzo.caceres@crunchdao.com'
 __url__ = 'https://github.com/crunchdao/crunch-cli'
