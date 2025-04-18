0f5b6d350360b12185df462319994b662428f1e9
Caceresenzo <caceresenzo1502@gmail.com>
2025-04-18 16:51:58 +0200

    fix: use correct test path

--- crunch/runner/cloud_executor.py
+++ crunch/runner/cloud_executor.py
@@ -188,16 +188,17 @@ class SandboxExecutor:
         test_path = next(iter(os.listdir(self.data_directory_path)), None)
         if not test_path:
             return  # no data?
+        test_path = os.path.join(self.data_directory_path, test_path)
 
         os.kill(self.fuse_pid, self.fuse_signal_number)
 
         time.sleep(0.1)
         for _ in range(10):
-            if not os.access(self.x_path, os.R_OK):
+            if not os.access(test_path, os.R_OK):
                 break
 
             time.sleep(1)
-            print("[debug] fuse not yet triggered", file=sys.stderr)
+            print(f"[debug] fuse not yet triggered - test_path=`{test_path}`", file=sys.stderr)
         else:
             print("fuse never triggered", file=sys.stderr)
             exit(1)
