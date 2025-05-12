772fd5403e203a5fabcf290782f9b7ac15cd8089
Caceresenzo <caceresenzo1502@gmail.com>
2025-05-12 18:55:56 +0200

    chore: remove debug code

--- crunch/cli.py
+++ crunch/cli.py
@@ -562,7 +562,6 @@ def cloud(
     max_retry: int,
     retry_seconds: int
 ):
-    print(f"gpu={gpu} getenv=", os.getenv('GPU', 'not set'))
     from .runner import is_inside
     if not is_inside:
         print("not in a runner")
