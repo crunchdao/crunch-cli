9803ea4d0fbbf3a8a76d8d1388f74618616ddcc1
Caceresenzo <caceresenzo1502@gmail.com>
2025-07-03 15:19:40 +0200

    release: bump to 7.0.5

--- crunch/__version__.py
+++ crunch/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'crunch-cli'
 __description__ = 'crunch-cli - CLI of the CrunchDAO Platform'
-__version__ = '7.0.4'
+__version__ = '7.0.5'
 __author__ = 'Enzo CACERES'
 __author_email__ = 'enzo.caceres@crunchdao.com'
 __url__ = 'https://github.com/crunchdao/crunch-cli'
