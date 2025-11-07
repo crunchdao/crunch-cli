d0284ad44c67531af243e2c4a6eb4d2cdf351a4b
Caceresenzo <caceresenzo1502@gmail.com>
2025-11-07 12:04:29 +0100

    feat(root): expose `container.Columns`

--- crunch/__init__.py
+++ crunch/__init__.py
@@ -8,4 +8,5 @@ your work to the crunchdao platform easily!
 import crunch.api as api
 import crunch.store as store
 from crunch.inline import load as load_notebook
+from crunch.container import Columns as Columns
 from crunch.runner import is_inside as is_inside_runner
