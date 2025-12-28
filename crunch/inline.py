1e900a6e70379ec356a8a5ee9212da0a5ddb53ef
Caceresenzo <caceresenzo1502@gmail.com>
2025-12-28 21:38:37 +0100

    feat(inline): add submit method

--- crunch/inline.py
+++ crunch/inline.py
@@ -1,18 +1,23 @@
+import json
 import logging
 import sys
 from functools import cached_property
+from textwrap import dedent
 from types import ModuleType
 from typing import Optional, Tuple, Union
 
 import click
 import pandas
 import psutil
+from IPython.display import Markdown, display
 
 import crunch.tester as tester
 from crunch.__version__ import __version__
-from crunch.api import Client, CompetitionFormat, CrunchNotFoundException, KnownData, MissingPhaseDataException, RoundIdentifierType
+from crunch.api import ApiException, Client, CompetitionFormat, CrunchNotFoundException, KnownData, MissingPhaseDataException, RoundIdentifierType
+from crunch.command.convert import convert
 from crunch.command.download import download, download_no_data_available
-from crunch.constants import DEFAULT_MODEL_DIRECTORY, DOT_PREDICTION_DIRECTORY
+from crunch.command.push import push
+from crunch.constants import DEFAULT_MAIN_FILE_PATH, DEFAULT_MODEL_DIRECTORY, DOT_PREDICTION_DIRECTORY
 from crunch.runner import is_inside
 from crunch.runner.types import KwargsLike
 from crunch.unstructured import RunnerModule, deduce_code_loader
@@ -164,6 +169,80 @@ class _Inline:
                 raise abort
 
         return None
+
+    def submit(
+        self,
+        message: Optional[str] = None,
+        model_directory_relative_path: str = DEFAULT_MODEL_DIRECTORY,
+        include_installed_packages_version: bool = False,
+        notebook_file_name: str = "notebook.ipynb",
+        main_file_name: str = DEFAULT_MAIN_FILE_PATH,
+    ):
+        if message is None:
+            message = input("Message: ")
+
+        try:
+            from google.colab import _message  # type: ignore
+            response = _message.blocking_request('get_ipynb', request='', timeout_sec=5)  # type: ignore
+
+            error = response.get("error")  # type: ignore
+            if error is not None:
+                raise NotImplementedError(f"{error.get('type')}: {error.get('description')}")  # type: ignore
+
+            ipynb = response.get("ipynb")  # type: ignore
+            if ipynb is None:
+                raise NotImplementedError(f"missing ipynb, available keys are: {list(response.keys())}")  # type: ignore
+
+            if ipynb.get("cells") is None:  # type: ignore
+                raise NotImplementedError(f"missing cells, available keys are: {list(ipynb.keys())}")  # type: ignore
+        except (ImportError, NotImplementedError) as error:
+            display(Markdown(dedent(f"""
+                Your work could not be submitted automatically, please do so manually:
+                1. Download your Notebook from Colab
+                2. Upload it to the platform
+                3. Create a run to validate it
+
+                ### >> [https://hub.crunchdao.com/competitions/{self._competition.name}/submit/notebook](https://hub.crunchdao.com/competitions/{self._competition.name}/submit/notebook?message=qsd)
+
+                <img alt="Download and Submit Notebook" src=https://raw.githubusercontent.com/crunchdao/competitions/refs/heads/master/documentation/animations/download-and-submit-notebook.gif height="600px" />
+
+                <br />
+                <small>Error preventing submit: <code>{error}</code></small>
+            """)))
+            return
+
+        with open(notebook_file_name, "w") as fd:
+            json.dump(ipynb, fd)
+
+        try:
+            convert(
+                notebook_file_path=notebook_file_name,
+                python_file_path=main_file_name,
+                override=True,
+            )
+        except SystemExit as error:
+            if error.code != 0:
+                print("conversion failed", file=sys.stderr)
+                return
+
+        try:
+            push(
+                message=message,
+                main_file_path=main_file_name,
+                model_directory_relative_path=model_directory_relative_path,
+                include_installed_packages_version=include_installed_packages_version,
+                dry=False,
+            )
+        except ApiException as error:
+            print("\n---")
+            error.print_helper()
+            return
+
+        display(Markdown(dedent(f"""
+            Next step is to run your submission in the cloud:
+
+            <img alt="Run in the Cloud" src=https://raw.githubusercontent.com/crunchdao/competitions/refs/heads/master/documentation/animations/create-run.gif height="600px" />
+        """)))
 
     @property
     def is_inside_runner(self):
